8562605ea997679db5c97f744059825d245b7a6e
Hiromi Sakurai <sakurai465270@gmail.com>
2018-07-13 20:35:34 +0900

    Added String+extensions

---
+++ RxQiita/Extensions/String+extensions.swift
@@ -0,0 +1,27 @@
+//
+//  String+extensions.swift
+//  RxQiita
+//
+//  Created by 櫻井寛海 on 2018/07/13.
+//  Copyright © 2018 Hiromi Sakurai. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func toDate() -> Date? {
+        let formmater = DateFormatter()
+        formmater.dateFormat = "yyyy-MM-dd"
+        return formmater.date(from: self)
+    }
+}
+
+extension Optional where Wrapped == String {
+    var nonNil: String {
+        if let s = self {
+            return s
+        } else {
+            return ""
+        }
+    }
+}
