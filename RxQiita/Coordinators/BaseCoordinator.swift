ee3adc7b08a9d8b18da95b4332f8ef84fbd5ecfd
Hiromi Sakurai <sakurai465270@gmail.com>
2018-08-01 15:37:29 +0900

    Added BaseCoordinator

---
+++ RxQiita/Coordinators/BaseCoordinator.swift
@@ -0,0 +1,40 @@
+//
+//  BaseCoordinator.swift
+//  RxQiita
+//
+//  Created by 櫻井寛海 on 2018/08/01.
+//  Copyright © 2018 Hiromi Sakurai. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class BaseCoordinator<ResultType> {
+    typealias CoordinationResult = ResultType
+
+    private let disposeBag = DisposeBag()
+
+    private let identifier = UUID()
+
+    private var childCoordinators: [UUID: Any] = [:]
+
+    private func store<T>(coordinator: BaseCoordinator<T>) {
+        childCoordinators[coordinator.identifier] = coordinator
+    }
+
+    private func free<T>(coordinator: BaseCoordinator<T>) {
+        childCoordinators[coordinator.identifier] = nil
+    }
+
+    func coordinate<T>(to coordinator: BaseCoordinator<T>) -> Observable<T> {
+        store(coordinator: coordinator)
+        return coordinator.start()
+            .do(onNext: { [weak self] _ in
+                self?.free(coordinator: coordinator)
+            })
+    }
+
+    func start() -> Observable<ResultType> {
+        fatalError("Start method should be implemented...")
+    }
+}
